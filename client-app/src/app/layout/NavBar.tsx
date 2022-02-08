838d9ac95f8725eac4fd84e61acf794f0681db9b
shoaib-ijaz <shoaib.ijaz8@gmail.com>
2022-02-08 16:53:35 +0500

    MobX implemented, removed the properties

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -5,11 +5,12 @@ import {
     Menu
 } from 'semantic-ui-react';
 
-interface Props {
-    openForm: () => void;
-}
+import { useStore } from '../stores/store';
 
-export default function NavBar({ openForm }: Props) {
+export default function NavBar() {
+
+    const {activityStore} = useStore();
+
     return (
         <Menu inverted fixed='top'>
             <Container>
@@ -19,7 +20,7 @@ export default function NavBar({ openForm }: Props) {
                 </Menu.Item>
                 <Menu.Item name='Activities' />
                 <Menu.Item>
-                    <Button onClick={openForm} positive content='Create Activity' />
+                    <Button onClick={()=>{activityStore.openForm()}} positive content='Create Activity' />
                 </Menu.Item>
             </Container>
         </Menu>
