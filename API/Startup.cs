976905c96ecbf8146818418682972a7387a9edec
shoaib-ijaz <shoaib.ijaz8@gmail.com>
2022-02-10 12:47:26 +0500

    Added the error handling module

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,6 +13,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Persistence;
 using API.Extensions;
+using FluentValidation.AspNetCore;
+using Application.Activities;
+using API.Middleware;
 
 namespace API
 {
@@ -28,17 +31,21 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers().AddFluentValidation(config =>
+            {
+                config.RegisterValidatorsFromAssemblyContaining<Create>();
+            });
 
-            services.AddControllers();
             services.AddApplicationServices(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.css';
 import 'react-calendar/dist/Calendar.css';
+import 'react-toastify/dist/ReactToastify.min.css';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import { store, StoreContext } from './app/stores/store';
-import { BrowserRouter } from 'react-router-dom';
+import {createBrowserHistory} from 'history';
+import { Router } from 'react-router-dom';
 // import reportWebVitals from './reportWebVitals';
+
+export const history = createBrowserHistory();
 
 ReactDOM.render(
     <StoreContext.Provider value={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+     <Router history={history}>
+      <App />
+    </Router>
     </StoreContext.Provider>,
     document.getElementById('root'));
 
