7b799c7da8734a082458906d268d3eb93735f6f7
shoaib-ijaz <shoaib.ijaz8@gmail.com>
2022-02-19 16:29:10 +0500

    Added the features: following, followers

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -176,14 +176,14 @@ export default class ActivityStore {
     }
 
     updateAttendeeFollowing = (username: string) => {
-        // this.activityRegistry.forEach(activity => {
-        //     activity.attendees.forEach(attendee => {
-        //         if (attendee.username === username) {
-        //             attendee.following ? attendee.followersCount-- : attendee.followersCount++;
-        //             attendee.following = !attendee.following;
-        //         }
-        //     })
-        // })
+        this.activityRegistry.forEach(activity => {
+            activity.attendees.forEach(attendee => {
+                if (attendee.username === username) {
+                    attendee.following ? attendee.followersCount-- : attendee.followersCount++;
+                    attendee.following = !attendee.following;
+                }
+            })
+        })
     }
 
     clearSelectedActivity = () => {
