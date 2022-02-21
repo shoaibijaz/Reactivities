2233fbdb266bb7836e2237b532649ad4f9413927
shoaib-ijaz <shoaib.ijaz8@gmail.com>
2022-02-21 13:38:46 +0500

    Added the facebook login feature

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,7 +1,7 @@
 import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Header, Segment, Image, Button } from 'semantic-ui-react';
+import { Container, Header, Segment, Image, Button, Divider } from 'semantic-ui-react';
 import { useStore } from '../../app/stores/store';
 import LoginForm from '../users/LoginForm';
 import RegisterForm from '../users/RegisterForm';
@@ -24,16 +24,25 @@ export default observer(function HomePage() {
                     </>
 
                 ) : (
-                        <>
-                            <Button onClick={() => modalStore.openModal(<LoginForm />)} size='huge' inverted>
-                                Login!
+                    <>
+                        <Button onClick={() => modalStore.openModal(<LoginForm />)} size='huge' inverted>
+                            Login!
                         </Button>
-                            <Button onClick={() => modalStore.openModal(<RegisterForm />)} size='huge' inverted>
-                                Register!
+                        <Button onClick={() => modalStore.openModal(<RegisterForm />)} size='huge' inverted>
+                            Register!
                         </Button>
-                        </>
+                        <Divider horizontal inverted>Or</Divider>
+                            <Button 
+                                loading={userStore.fbLoading}
+                                size='huge'
+                                inverted
+                                color='facebook'
+                                content='Login with Facebook'
+                                onClick={userStore.facebookLogin}
+                            />
+                    </>
 
-                    )}
+                )}
             </Container>
         </Segment>
     )
